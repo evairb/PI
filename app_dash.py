677c0b34c7fde8d893c97ed612f92f173e02e332
Evair Bernardino <evairbd@gmail.com>
2023-05-25 17:18:48 -0300

    first commit

---
+++ app_dash.py
@@ -0,0 +1,65 @@
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
+import pandas as pd
+
+stylesheets = ["./assets/style/style.css"]
+scripts = ["./assets/js/main.js"]
+app = Dash(__name__, external_stylesheets=stylesheets, external_scripts=scripts)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv("desmatamento.csv")
+
+dfe = pd.read_csv("desmatamento-estado.csv")
+dfe = dfe.sort_values(by="area km²", ascending=False )
+fig = px.bar(dfe, x="uf",  y="area km²",text_auto='.2s')
+
+
+
+
+
+
+opcoes = list(df['Estado'].unique())
+
+opcoes.append("Todos Estados")
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Desmatamento Mata Atlântica', id="Titulo" ),
+    html.H2(children='Contem apenas estados que contem mata atlântica', id="Explicacao1" ),
+    html.H3(children='Municipios com indice de desmatamento menor que 0,1 km não foram computados', id="Explicacao2" ),
+    html.Div(children='''
+        Selecione o municipio desejado
+    '''),
+    
+    html.Div(id="texto"),
+    
+
+
+
+    dcc.Dropdown(opcoes, value='Todos Estados', id='select-Municipio'),
+    
+    
+    dcc.Graph(
+        id='Grafico',
+        figure=fig
+    )
+    
+])
+
+
+@app.callback(
+    Output('Grafico', 'figure'),
+    Input('select-Municipio', 'value')
+)
+
+def teste(value):
+    if value == "Todos Estados":
+        fig = px.bar(dfe, x="uf",  y="area km²")
+    else:
+        tabela_filtrada = df.loc[df['Estado']==value,:].sort_values(by="Area Desmatada(km)", ascending = False )
+        fig = px.bar(tabela_filtrada, x="Municipio",  y="Area Desmatada(km)")
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
